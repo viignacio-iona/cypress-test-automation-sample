c4c6e0d8e6a9d65e66099a7af1070be09780e446
viignacio-iona <van.ignacio@ionacommerce.com>
2026-02-05 15:21:01 +0800

    Added sign up modal display and valid sign up tests.

--- cypress/e2e/Van/signup.cy.ts
+++ cypress/e2e/Van/signup.cy.ts
@@ -1,8 +1,39 @@
-describe('Signup Flow', () => {
-  // TODO: Add signup test cases here
-  // Test scenarios to implement:
+describe('Signup tests', () => {
+  describe('Signup tests', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+    
   // - Signup modal opens correctly
+  it('should open the signup modal', () => {
+    cy.get('#narvbarx').within(() => {
+      cy.get('[data-target="#signInModal"]').click();
+    });
+
+    cy.get('#signInModal').should('be.visible').contains('Sign up');
+  });
+
   // - Valid signup with new username and password
+  it('should signup with new username and password', () => {
+    const randomEmail = Cypress.generateRandomEmail();
+
+    cy.window().then((win) => {
+      cy.stub(win as any, 'alert').as('alertStub');
+    });
+
+    cy.get('#narvbarx').within(() => {
+      cy.get('[data-target="#signInModal"]').click();
+    });
+
+    cy.get('#signInModal').within(() => {
+      cy.get('#sign-username').clear().invoke('val', randomEmail);
+      cy.get('#sign-password').clear().invoke('val', 'test');
+      cy.contains('button', 'Sign up').click();
+    });
+
+    cy.get('@alertStub').should('have.been.calledWith', 'Sign up successful.');
+  });
+
   // - Username already exists validation
   // - Empty username field validation
   // - Empty password field validation
@@ -10,4 +41,5 @@ describe('Signup Flow', () => {
   // - Password requirements (if applicable)
   // - Successful signup creates account
   // - Signup modal closes correctly
+  });
 });
